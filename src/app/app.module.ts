3aa97650d2e0c611ca439392bef685d83c8cac3c
justin burnel <justin.burnel@gmail.com>
2023-01-02 22:31:03 +0100

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { FiltersComponent } from './pages/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/components/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CartService } from './services/cart.service';
+import { StoreService } from "./services/store.service";
 
 @NgModule({
   declarations: [
@@ -56,7 +57,7 @@ import { CartService } from './services/cart.service';
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  providers: [CartService],
+  providers: [CartService, StoreService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
